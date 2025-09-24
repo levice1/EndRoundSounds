cdddfab6959b6fcea4c0e1a7f976693ca3e39552
Andrii <124053072+levice1@users.noreply.github.com>
2025-09-24 12:31:23 +0200

    Update EndRoundSoundsPlugin.cs
    Add Volume parameter and executing snd_toolvolume command.

--- EndRoundSounds/EndRoundSoundsPlugin.cs
+++ EndRoundSounds/EndRoundSoundsPlugin.cs
@@ -1,6 +1,7 @@
-﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
+using System.Globalization;
 
 namespace EndRoundSounds;
 
@@ -8,27 +9,29 @@ namespace EndRoundSounds;
 public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConfig>
 {
     public override string ModuleName => "End Round Sounds Plugin";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.0.1";
     public override string ModuleAuthor => "GianniKoch";
-    public override string ModuleDescription => "A plugin that plays configurable sounds at the end of each round.";
+    public override string ModuleDescription => "A plugin that plays configurable sounds at the end of each round.(Modified)";
 
     public required EndRoundSoundsConfig Config { get; set; }
 
     public override void Load(bool hotReload)
     {
         RegisterEventHandler<EventRoundEnd>(OnRoundEnd);
+        RegisterEventHandler<EventRoundStart>(OnRoundStart);
         Server.PrintToConsole("Loaded End Round Sounds Plugin!");
     }
 
     public void OnConfigParsed(EndRoundSoundsConfig config)
     {
         Config = config;
-        Server.PrintToConsole($"Found {Config.Sounds.Count} sounds!");
+        Server.PrintToConsole($"Found {Config.Sounds.Count} sounds! Volume: {Config.Volume}");
     }
 
     public override void Unload(bool hotReload)
     {
         DeregisterEventHandler<EventRoundEnd>(OnRoundEnd);
+        DeregisterEventHandler<EventRoundStart>(OnRoundStart);
         Server.PrintToConsole("Unloaded End Round Sounds Plugin!");
     }
 
@@ -38,19 +41,34 @@ public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConf
         {
             return HookResult.Continue;
         }
-        
+
         var song = Config.Sounds[Random.Shared.NextDistinct(Config.Sounds.Count)];
+        float vol = Math.Clamp(Config.Volume, 0f, 1f);
+        string volStr = vol.ToString(CultureInfo.InvariantCulture);
         var players = Utilities.GetPlayers();
+
         foreach (var player in players)
         {
+            player.ExecuteClientCommand($"snd_toolvolume {volStr}");
             player.ExecuteClientCommand($"play \"{song}\"");
         }
 
         return HookResult.Continue;
     }
+    
+    private HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        var players = Utilities.GetPlayers();
+        foreach (var player in players)
+        {
+            player.ExecuteClientCommand("snd_toolvolume 1.0");
+        }
+        return HookResult.Continue;
+    }
 }
 
-public class EndRoundSoundsConfig: BasePluginConfig
+public class EndRoundSoundsConfig : BasePluginConfig
 {
     public List<string> Sounds { get; set; } = [];
+    public float Volume { get; set; } = 1.0f;
 }
